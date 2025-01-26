6156f949477c2d2fbd39f54cf4a928310511d094
naingkhanthtet <naingkhanthtet15@gmail.com>
2025-01-26 21:45:36 +0700

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:reader/models/book.dart';
+import 'package:reader/network/network.dart';
+import 'package:reader/pages/favorite_page.dart';
+import 'package:reader/pages/home_page.dart';
+import 'package:reader/pages/saved_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -28,30 +33,49 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    const HomePage(),
+    const FavoritePage(),
+    const SavedPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Reader'),
-        backgroundColor: Theme.of(context).colorScheme.primary,
+      // appBar: AppBar(
+      //   title: const Text('Reader'),
+      //   backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      // ),
+      body: Center(
+        child: _screens[_currentIndex],
       ),
-      body: const Center(
-        child: Text('Hello, World!'),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.save),
+            label: 'Save',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorite',
+          ),
+        ],
+        selectedItemColor: Theme.of(context).colorScheme.onInverseSurface,
+        unselectedItemColor: Theme.of(context).colorScheme.onSurface,
+        onTap: (value) {
+          setState(() {
+            _currentIndex = value;
+          });
+        },
       ),
-      bottomNavigationBar: BottomNavigationBar(items: <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'Home',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.save),
-          label: 'Save',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.favorite),
-          label: 'Favorite',
-        ),
-      ]),
     );
   }
 }
