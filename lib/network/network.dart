b099a11229a8ee3339eb3ef845c392e13ad3ed04
naingkhanthtet <naingkhanthtet15@gmail.com>
2025-01-26 21:44:42 +0700

    Book api getter

---
+++ lib/network/network.dart
@@ -0,0 +1,26 @@
+import 'package:reader/models/book.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Network {
+  static const String _baseUrl = 'https://www.googleapis.com/books/v1/volumes';
+
+  Future<List<Book>> getBooks(String query) async {
+    var url = Uri.parse('$_baseUrl?q=$query');
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      var data = json.decode(response.body);
+      if (data['items'] != null && data['items'] is List) {
+        List<Book> books = (data['items'] as List<dynamic>)
+            .map((book) => Book.fromJson(book as Map<String, dynamic>))
+            .toList();
+        return books;
+      } else {
+        return [];
+      }
+    } else {
+      throw Exception('Failed to load books');
+    }
+  }
+}
