8aa74437ae177ede9aae172419fc15a60886b9f6
naingkhanthtet <naingkhanthtet15@gmail.com>
2025-01-27 21:55:54 +0700

    Saved Page

--- lib/pages/saved_page.dart
+++ lib/pages/saved_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:reader/db/db_helper.dart';
+import 'package:reader/models/book.dart';
+import 'package:reader/utils/book_details_arguments.dart';
 
 class SavedPage extends StatefulWidget {
   const SavedPage({super.key});
@@ -11,12 +14,55 @@ class _SavedPageState extends State<SavedPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Reader'),
-      ),
-      body: const Center(
-        child: Text('Saved'),
-      ),
-    );
+        appBar: AppBar(),
+        body: FutureBuilder(
+          future: DbHelper.instance.readAllBooks(),
+          builder: (context, snapshot) => snapshot.hasData
+              ? ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    Book book = snapshot.data![index];
+                    return InkWell(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/details',
+                              arguments: BookDetailsArguments(
+                                  itemBook: book, isSaved: true));
+                        },
+                        child: Card(
+                            child: ListTile(
+                          leading: Image.network(
+                            book.imageLinks['thumbnail'] ?? '',
+                            fit: BoxFit.cover,
+                          ),
+                          title: Text(book.title),
+                          subtitle: Column(
+                            children: [
+                              // Text(book.authors.join(', ')),
+                              ElevatedButton.icon(
+                                onPressed: () async {
+                                  book.isFavorite = !book.isFavorite;
+                                  await DbHelper.instance
+                                      .toggleFavorite(book.id, book.isFavorite);
+                                  setState(() {});
+                                },
+                                icon: Icon(
+                                  book.isFavorite
+                                      ? Icons.favorite
+                                      : Icons.favorite_outline,
+                                  color: book.isFavorite ? Colors.red : null,
+                                ),
+                                label: Text((book.isFavorite)
+                                    ? 'Favorite'
+                                    : 'Add to Favorites'),
+                              ),
+                            ],
+                          ),
+                        )));
+                  },
+                )
+              : const Center(
+                  child: CircularProgressIndicator(),
+                ),
+        ));
   }
 }
